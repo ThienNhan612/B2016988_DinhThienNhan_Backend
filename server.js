7c5d576974979fee4bf1f5d29747eaa32faedbf1
ThienNhan612 <142494680+ThienNhan612@users.noreply.github.com>
2023-09-20 07:23:53 +0700

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const config = require("./app/config");
 
-const PORT = config.app.PORT;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+const port = config.app.port;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
 });
