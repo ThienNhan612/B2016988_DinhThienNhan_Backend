729e0aa8ad3d1dda13d252e6e304c116222c02f1
ThienNhan612 <nhanb6988@gmail.com>
2023-10-04 08:12:49 +0700

    Cai dat cac handler truy xuat CSDL

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -3,26 +3,28 @@ const ContactService = require("../services/contact.service");
 const MongoDB = require("../utils/mongodb.until");
 
 exports.create = async (req, res, next) => {
-  if (!req.body?.name) {
-    return next(new ApiError(400, "Name cannot be empty"));
-  }
-  try {
-    const contactService = new ContactService(MongoDB.client); // Sửa tên biến từ "ContactService" thành "contactService"
-    const document = await contactService.create(req.body); // Sửa "ContactService" thành "contactService"
-    return res.send(document);
-  } catch (error) {
-    return next(new ApiError(500, "An error occurred while creating the contact"));
-  }
+    if (!req.body?.name) {
+        return next(new ApiError(400, "Name cannot be empty"));
+    }
+    try {
+        const contactService = new ContactService(MongoDB.client); // Sửa tên biến từ "ContactService" thành "contactService"
+        const documents = await contactService.create(req.body); // Sửa "ContactService" thành "contactService"
+        return res.send(documents);
+    } catch (error) {
+        return next(
+            new ApiError(500, "An error occurred while creating the contact")
+        );
+    }
 };
 exports.findAll = async (req, res, next) => {
     let documents = [];
     try{
-        const ContactService = new ContactService(MongoDB.client);
+        const contactService = new ContactService(MongoDB.client);
         const{name} = req.query;
         if(name){
-            documents = await ContactService.findByName(name);
+            documents = await contactService.findByName(name);
         }else{
-            documents = await ContactService.find({});
+            documents = await contactService.find({});
         }
 
     }catch(error){
@@ -34,8 +36,8 @@ exports.findAll = async (req, res, next) => {
 };
 exports.findOne = async (req, res, next) =>{
     try{
-        const ContactService = new ContactService(MongoDB.client);
-        const document = await ContactService.findByid(req.params.id);
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.findByid(req.params.id);
         if (!document);
     
     }catch (error){
@@ -52,8 +54,8 @@ exports.update = async (req, res, next) => {
         return next (new ApiError(400, "Data to update cannot be empty"));
     }
     try{
-        const ContactService = new ContactService(MongoDB.client);
-        const document = await ContactService.update(req.params.id, req.body);
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.update(req.params.id, req.body);
         if (!document){
             return next(new ApiError(404, "Contact not found"));
         }
@@ -66,8 +68,8 @@ exports.update = async (req, res, next) => {
 };
 exports.delete =async (req, res, next) => {
     try{
-        const ContactService = new ContactService(MongoDB.client);
-        const document = await ContactService.delete(req.params.id);
+        const contactService = new ContactService(MongoDB.client);
+        const document = await contactService.delete(req.params.id);
         if (!document){
             return next(new ApiError(404, "Contact not found"));
         }
@@ -83,8 +85,8 @@ exports.delete =async (req, res, next) => {
 };
 exports.deleteAll =async (req, res, next) => {
     try{
-        const ContactService = new ContactService(MongoDB.client);
-        const deletedCount = await ContactService.deleteAll();
+        const contactService = new ContactService(MongoDB.client);
+        const deletedCount = await contactService.deleteAll();
         return res.send({
             message: `${deletedCount} contacts were deleted succesfully`,
         });
@@ -96,8 +98,8 @@ exports.deleteAll =async (req, res, next) => {
 };
 exports.findAllFavorite =async (req, res, next) => {
    try{
-      const ContactService = new ContactService(MongoDB.client);
-      const documents = await ContactService.findAllFavorite();
+      const contactService = new ContactService(MongoDB.client);
+      const documents = await contactService.findAllFavorite();
       return res.send(documents);
    }catch(error){
     return next(
--- app/services/contact.service.js
+++ app/services/contact.service.js
@@ -6,34 +6,29 @@ class ContactService {
   }
 
   // Định nghĩa các phương thức truy xuất CSDL sử dụng mongodb API
-  extractContactData(payload) {
+extractConactData(payload) {
     const contact = {
       name: payload.name,
       email: payload.email,
       address: payload.address,
       phone: payload.phone,
       favorite: payload.favorite,
-    };
-
-    // Xóa các trường không xác định
-    Object.keys(contact).forEach((key) => {
-      if (contact[key] === undefined) {
-        delete contact[key];
-      }
-    });
-
-    return contact;
-  }
-
+  };
+// Remove undefined fields
+    Object.keys(contact).forEach(
+      (key) => contact[key] === undefined && delete contact[key]
+  );
+  return contact;
+}
   async create(payload) {
-    const contact = this.extractContactData(payload);
+    const contact = this.extractConactData(payload);
     const result = await this.Contact.findOneAndUpdate(
-      { name: contact.name },
-      { $set: contact, $setOnInsert: { favorite: false } },
-      { returnDocument: "after", upsert: true }
-    );
-    return result.value;
-  }
+      contact,
+        { $set: { favorite: contact.favorite === true } },
+        { returnDocument: "after", upsert: true }
+  );
+  return result.value;
+}
 
   async find(filter) {
     const cursor = await this.Contact.find(filter);
