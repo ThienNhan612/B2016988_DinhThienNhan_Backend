6f10ee12917030a4a23f11e0eb4fcaa12dea1b6b
ThienNhan612 <nhanb6988@gmail.com>
2023-11-01 07:40:37 +0700

    update

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <ContactForm :contact="contactData" 
+      @submit:contact="CreateContact"
+      />
+      <h1></h1>
+    </div>
+  </template>
+  
+  <script>
+  import ContactForm from "@/components/ContactForm.vue";
+  import ContactService from "@/services/contact.service";
+  
+  export default {
+    components: {
+      ContactForm,
+    },
+    data() {
+      return {
+        contactData: {
+          name: "",
+          email: "",
+          address: "",
+          phone: "",
+          favorite: false,
+        },
+      };
+    },
+    methods:{
+        async CreateContact(data) {
+        try {
+            await ContactService.create(data)
+            confirm('tạo hiện hệ thành công')
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }
+  };
+  </script>
